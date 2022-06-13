b73874a8e27340f3cc9ab6e959fd0de674850078
pyade <yp9106@outlook.com>
2022-06-13 16:53:10 +0800

    initial commit

---
+++ lisp/bui.el/examples/buffers.el
@@ -0,0 +1,195 @@
+;;; buffers.el --- List of buffers and buffer info
+
+;; Copyright © 2016–2017, 2021 Alex Kost <alezost@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is an example of using BUI (Buffer User Interface) library.
+;;
+;; It shows how to use bui to display a list of buffers à la
+;; `list-buffers' or `ibuffer'.
+;;
+;; To try it, load this file (for example, with "M-x load-file"), and
+;; run "M-x buffers" command.  There you can mark several buffers (with
+;; "m") and press "i" to display the info buffer; press "f f" to enable
+;; filters, etc.  Press "h" to look at the "hint" (available keys).
+
+;;; Code:
+
+(require 'bui)
+(require 'help-mode)  ; for `help-function' button
+
+(bui-define-groups buffers
+  :parent-group tools
+  :parent-faces-group faces
+  :group-doc "Settings for '\\[buffers]' command."
+  :faces-group-doc "Faces for '\\[buffers]' command.")
+
+(defun buffers-get-buffers (&optional search-type &rest search-values)
+  (or search-type (setq search-type 'all))
+  (cl-case search-type
+    (all (buffer-list))
+    (id search-values)
+    (t (error "Unknown search type: %S" search-type))))
+
+(defun buffers-buffer-file-name ()
+  (or buffer-file-name
+      (and (boundp 'dired-directory)
+           (if (stringp dired-directory)
+               dired-directory
+             (car dired-directory)))))
+
+(defun buffers-visited-file-modtime ()
+  (let ((time (visited-file-modtime)))
+    (cl-case time
+      ((-1 0) nil)
+      (t time))))
+
+(defun buffers-buffer->entry (buffer)
+  (with-current-buffer buffer
+    `((id   . ,buffer)
+      (name . ,(buffer-name))
+      (mode . ,major-mode)
+      (size . ,(buffer-size))
+      (mod-time  . ,(buffers-visited-file-modtime))
+      (file-name . ,(buffers-buffer-file-name)))))
+
+(defun buffers-get-entries (&rest args)
+  (mapcar #'buffers-buffer->entry
+          (apply #'buffers-get-buffers args)))
+
+(bui-define-entry-type buffers
+  :titles '((mod-time . "Modification Time"))
+  :get-entries-function #'buffers-get-entries
+  :filter-predicates
+  (append bui-filter-predicates
+          '(buffers-buffer-ephemeral?
+            buffers-buffer-non-ephemeral?
+            buffers-buffer-visiting-file?
+            buffers-buffer-not-visiting-file?)))
+
+(defun buffers-describe-mode-function (button)
+  (describe-function (intern (button-label button))))
+
+
+;;; Filter predicates
+
+(defun buffers-buffer-ephemeral? (entry)
+  "Return non-nil, if ENTRY's buffer name starts with a space."
+  (string= " " (substring (bui-entry-value entry 'name) 0 1)))
+
+(defun buffers-buffer-non-ephemeral? (entry)
+  "Return non-nil, if ENTRY's buffer name does not start with a space."
+  (not (buffers-buffer-ephemeral? entry)))
+
+(defun buffers-buffer-visiting-file? (entry)
+  "Return non-nil, if ENTRY's buffer visits a file."
+  (bui-entry-non-void-value entry 'file-name))
+
+(defun buffers-buffer-not-visiting-file? (entry)
+  "Return non-nil, if ENTRY's buffer does not visit a file."
+  (not (buffers-buffer-visiting-file? entry)))
+
+
+;;; 'Info' interface
+
+(bui-define-interface buffers info
+  :format '((name format buffers-info-insert-name)
+            (mode format (simple buffers-mode-function))
+            (size format (format))
+            nil
+            (file-name nil (simple bui-file))
+            (mod-time format (time))))
+
+(define-button-type 'buffers-mode-function
+  :supertype 'help-function
+  'action 'buffers-describe-mode-function)
+
+(defun buffers-info-insert-name (name entry)
+  (bui-info-insert-value-simple (bui-entry-value entry 'name)
+                                'mode-line-buffer-id)
+  (bui-insert-indent)
+  (bui-insert-action-button
+   "Switch"
+   (lambda (btn)
+     (pop-to-buffer (button-get btn 'buffer)))
+   "Switch to this buffer"
+   'buffer (bui-entry-id entry)))
+
+
+;;; 'List' interface
+
+(bui-define-interface buffers list
+  :buffer-name "*Buffers*"
+  :describe-function #'buffers-list-describe
+  :titles '((mod-time . "Mod. Time"))
+  :format '((name nil 30 t)
+            (mode buffers-list-get-mode 25 t)
+            (size nil 8 bui-list-sort-numerically-2 :right-align t)
+            ;; (mod-time bui-list-get-time 20 t)
+            (file-name bui-list-get-file-name 30 t))
+  :hint 'buffers-list-hint
+  :sort-key '(name))
+
+(let ((map buffers-list-mode-map))
+  (define-key map (kbd "RET") 'buffers-list-switch-to-buffer)
+  (define-key map (kbd "k")   'buffers-list-kill-buffers))
+
+(defvar buffers-list-default-hint
+  '(("\\[buffers-list-switch-to-buffer]") " switch to buffer;\n"
+    ("\\[buffers-list-kill-buffers]") " kill buffer(s);\n"))
+
+(defun buffers-list-hint ()
+  "Return a hint string to display in the echo area."
+  (bui-format-hints
+   buffers-list-default-hint
+   (bui-default-hint)))
+
+(defun buffers-list-get-mode (mode &optional _)
+  "Return MODE button specification for `tabulated-list-entries'.
+MODE may be nil."
+  (list (symbol-name mode)
+        :supertype 'help-function
+        'action 'buffers-describe-mode-function))
+
+(defun buffers-list-describe (&rest buffers)
+  "Display 'info' buffer for BUFFERS."
+  (bui-get-display-entries 'buffers 'info (cons 'id buffers)))
+
+(defun buffers-list-switch-to-buffer ()
+  (interactive)
+  (pop-to-buffer (bui-list-current-id)))
+
+(defun buffers-list-kill-buffers ()
+  "Kill marked buffers (or the current buffer)."
+  (interactive)
+  (dolist (buffer (or (bui-list-get-marked-id-list)
+                      (list (bui-list-current-id))))
+    (kill-buffer buffer))
+  (revert-buffer nil t))
+
+
+;;; Interactive commands
+
+;;;###autoload
+(defun buffers ()
+  "Display a list of buffers."
+  (interactive)
+  (bui-list-get-display-entries 'buffers))
+
+(provide 'buffers)
+
+;;; buffers.el ends here
---
+++ settings.el
@@ -0,0 +1,274 @@
+(setq package-enable-at-startup nil)
+(setq package-archives '(("gnu"   . "http://mirrors.163.com/elpa/gnu/")
+                         ("melpa" . "http://mirrors.163.com/elpa/melpa/")
+                         ("org"   . "http://orgmode.org/elpa/")))
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(global-linum-mode 1)
+(scroll-bar-mode -1)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/bui.el")
+(require 'bui)
+
+(defun buffers-buffer->entry (buffer)
+  (with-current-buffer buffer
+    `((id   . ,buffer)
+      (name . ,(buffer-name))
+      (mode . ,major-mode)
+      (size . ,(buffer-size))
+      (file-name . ,buffer-file-name))))
+
+(defun buffers-get-entries ()
+  (mapcar 'buffers-buffer->entry (buffer-list)))
+
+(bui-define-interface buffers list
+  :buffer-name "*Buffers*"
+  :get-entries-function 'buffers-get-entries
+  :format '((name nil 30 t)
+            (mode nil 25 t)
+            (size nil 8 bui-list-sort-numerically-2 :right-align t)
+            (file-name bui-list-get-file-name 30 t))
+  :sort-key '(name))
+
+(defun buffers ()
+  "Display a list of buffers."
+  (interactive)
+  (bui-get-display-entries 'buffers 'list))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(add-to-list 'load-path "~/.emacs.d/lisp/avy")
+(require 'avy)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/emacsql/")
+(require 'emacsql)
+
+(recentf-mode 1)
+(global-set-key (kbd "C-x p") 'recentf-open-files)
+(global-set-key [f8] 'recentf-open-files)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/yasnippet")
+(require 'yasnippet)
+(add-to-list 'load-path "~/.emacs.d/lisp/auto-yasnippet")
+(require 'auto-yasnippet)
+(add-to-list 'load-path "~/.emacs.d/lisp/yasnippet-snippets")
+(require 'yasnippet-snippets)
+
+(yas-global-mode +1)
+(global-set-key (kbd "C-c i") 'yas-insert-snippet)
+
+(defun indent-buffer()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(global-set-key (kbd "C-M-\\") 'indent-buffer)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/ace-window")
+(require 'ace-window)
+(setq aw-dispatch-always t)
+(global-set-key (kbd "C-x o") 'ace-window)
+(defvar aw-dispatch-alist
+  '((?d aw-delete-window "Delete Window")
+    (?D delete-other-windows "Delete Other Windows")
+    (?m aw-swap-window "Swap Windows")
+    (?M aw-move-window "Move Window")
+    (?c aw-copy-window "Copy Window")
+    (?j aw-switch-buffer-in-window "Select Buffer")
+    (?n aw-flip-window "Flip Window")
+    (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+    (?f aw-split-window-fair "Split Fair Window")
+    (?h aw-split-window-vert "Split Vertical")
+    (?v aw-split-window-horz "Split Horizontal")
+    (?? aw-show-dispatch-help))
+  "List of actions for `aw-dispatch-default'.")
+
+(global-set-key [f7] 'toggle-input-method)
+
+(add-to-list 'load-path
+             "~/.emacs.d/lisp/emacs-rime")
+(require 'rime)
+
+(setq rime-user-data-dir "~/.config/fcitx5/rime")
+(setq default-input-method "rime")
+
+(setq rime-show-candidate 'posframe)
+
+(add-to-list 'load-path
+             "~/.emacs.d/lisp/posframe")
+(require 'posframe)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/auctex")
+
+(add-to-list 'load-path "~/.emacs.d/lisp/doct/")
+(require 'doct)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/org-protocol-capture-html/")
+(require 'org-protocol-capture-html)
+
+(setq org-hide-leading-stars t)
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "|" "DONE(d)")
+        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
+        (sequence "|" "CANCELED(c)")))
+
+(add-to-list 'load-path "~/.emacs.d/lisp/deft/")
+(require 'deft)
+
+(setq deft-extensions '("txt" "tex" "org"))
+(setq deft-directory "~/braindump")
+(setq deft-recursive t)
+(setq deft-use-filename-as-title t)
+(setq deft-strip-summary-regexp
+      (concat "\\("
+              "^:.+:.*\n" ; any line with a :SOMETHING:
+              "\\|^#\\+.*\n" ; anyline starting with a #+
+              "\\|^\\*.+.*\n" ; anyline where an asterisk starts the line
+              "\\)"))
+(global-set-key [f3] 'deft)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/org-roam/")
+(require 'org-roam)
+
+(setq org-roam-directory (file-truename "~/braindump"))
+
+(setq org-roam-db-location (file-truename "~/braindump/org-roam.db"))
+(setq org-roam-v2-ack t)
+(org-roam-db-autosync-mode)
+
+(setq org-roam-keymap
+      '( keymap 
+         ( ?m . org-roam-mode)		           ;m
+         ( 59 . org-roam-buffer-toggle)	           ;;
+         ( ?: . org-roam-buffer-display-dedicated) ;:
+         ( ?c . org-roam-capture)		   ;c
+         ( ?' . org-roam-alias-add)		   ;'
+         ( ?r . org-roam-node-random)		   ;r
+         ( ?g . org-roam-graph)		   ;g
+         ( ?d  keymap				   ;d
+           ( ?d . org-roam-dailies-capture-date)  ;dd
+           ( ?t . org-roam-dailies-capture-today) ;dt
+           ( ?f . org-roam-dailies-find-directory)) ;df
+         ( ?t . org-roam-tag-add)		    ;t
+         ( ?T . org-roam-tag-delete)		    ;T
+         ( ?f . org-roam-node-find)		    ;f
+         ( ?i . org-roam-node-insert)		    ;i
+         ))
+
+;; Inbox
+(setq pei/inbox-file "~/braindump/inbox.org")
+(setq org-capture-templates
+      `(("i" "inbox" entry (file pei/inbox-file)
+         "* TODO %?")
+        ("w" "Web site" entry (file pei/inbox-file)
+         "* %a :website:\n\n%U %?\n\n%:initial")))
+
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-overwrite-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; SPC j/k will run the original command in MOTION state.
+   '("j" . "H-j")
+   '("k" . "H-k")
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet)
+   (cons "r" org-roam-keymap))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)))
+
+(add-to-list 'load-path
+             "~/.emacs.d/lisp/meow")
+(require 'meow)
+(setq meow-keyboard-describe-delay 0.1)
+(setq meow-cheatsheet-physical-layout meow-cheatsheet-physical-layout-ansi)
+(meow-setup)
+(meow-setup-indicator)
+(meow-global-mode 1)
